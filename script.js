a6770e57935cc39793143ebea5573fe1f3f6f4b0
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-11-12 19:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var isDate = function (input) {
-  //   write your code here
-};
+function checkDate() {
+  const dateInput = document.getElementById('dateInput').value;
+  const resultElement = document.getElementById('result');
+  
+  if (isDate(dateInput)) {
+    resultElement.textContent = 'Valid Date!';
+    resultElement.style.color = '#4caf50';  // Green color for success
+  } else {
+    resultElement.textContent = 'Invalid Date!';
+    resultElement.style.color = '#e74c3c';  // Red color for failure
+  }
+}
 
-// Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+function isDate(input) {
+  // The same isDate function as mentioned in the previous code snippet
+}
